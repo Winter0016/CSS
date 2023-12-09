9e8e2eb7c172120be871e605b83c57ce5b44133f
Winter0016 <chauquangphuc2604.2604@gmail.com>
2023-12-09 23:45:08 +0700

    I  ALMOST DID IT, MORE THAN HALF WAY FOR COOKIE!

--- SOME  CSS , CSS + JS/add to cart/index.js
+++ SOME  CSS , CSS + JS/add to cart/index.js
@@ -1,6 +1,7 @@
 let Item;
 let Price;
 let Item_name;
+let cart_list =document.querySelector('.my-list');
 
 // Define the setCookie function
 function setCookie(something, value, daysToLive) {
@@ -25,6 +26,45 @@ function deleteAllCookies() {
 function deleteCookie(name) {
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
 }
+
+function Lastfunction() {
+    let target = getCookieItemName();
+
+    for (let i = 0; i < target.length; i++) {
+        console.log("first for");
+        let indexOfB = target[i].indexOf("=");
+        let real_target = target[i].substring(0, indexOfB);
+        let real_target_number = getCookie(`Count_${real_target}`);
+        let real_target_price = getCookie(`Price_${real_target}`);
+        console.log(`real target: ${real_target}`);
+
+        let classFound = false;
+
+        for (let y = 0; y < cart_list.children.length; y++) {
+            console.log("Enter second for");
+            let child = cart_list.children[y];
+
+            if (child.classList.contains(real_target)) {
+                classFound = true;
+                child.textContent = `${real_target} : ${real_target_number} (${real_target_price})`;                break;
+            }
+        }
+
+        if (!classFound) {
+            let List = document.createElement("li");
+            List.classList.add(`${real_target}`);
+            List.textContent = `${real_target} : ${real_target_number} (${real_target_price})`;
+            console.log("children.length = 0 or class not found");
+            cart_list.appendChild(List);
+        }
+    }
+}
+
+window.onload = function () {
+    Lastfunction();
+};
+
+
 
 //deleteAllCookies(); //delete all cookies by default;
 
@@ -58,6 +98,7 @@ document.querySelectorAll('.buy').forEach(function(anchor) {
 
         // Log the current cookies
         console.log(document.cookie);
+        Lastfunction();
     });
 });
 
@@ -80,7 +121,48 @@ function getCookie(name) {
     return null;
 }
 
+function getCookieItemName() {
+    //console.log(`cookies before split: `,document.cookie);
+    const cookies = document.cookie.split(';');
+    //console.log(`cookies after split: ${cookies}`);
+    //console.log(`cookies length: ${cookies.length}`);
+    let temp =[];
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        //console.log(`cookies${i} : ${cookie}`);
+        if (cookie.startsWith("Count_")) {
+            console.log(`cookie: ${cookie}`);
+            temp.push(cookie.substring(6));
+        }
+    }
+    return temp;
+}
+
+
 let opencart = document.querySelector(".cart-item");
 opencart.addEventListener('click', function() {
     // Add code to handle the cart item click
 });
+
+//////////start coding for My cart///
+let cart_button = document.querySelector('.cart-item');
+let close = true;
+
+cart_button.addEventListener('click',function(){
+    if(cart_list.style.display=="none"|| close==true){
+        cart_list.style.display="block";
+        close = false;
+    }
+    else{
+        cart_list.style.display="none";
+    }
+
+});
+
+    /////// get cookie to send to the cart_list/////
+
+
+
+
+
+
