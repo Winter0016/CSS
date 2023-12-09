1df754919e955e31d981e32b738177f5504866af
Winter0016 <chauquangphuc2604.2604@gmail.com>
2023-12-09 11:25:35 +0700

    THEM COOKIE

--- SOME  CSS , CSS + JS/add to cart/index.js
+++ SOME  CSS , CSS + JS/add to cart/index.js
@@ -1,6 +1,8 @@
 let Item;
 let Price;
 let Item_name;
+let Item_count = 0;
+let Item_number = 0;
 
 // Define the setCookie function
 function setCookie(something, value, daysToLive) {
@@ -11,6 +13,7 @@ function setCookie(something, value, daysToLive) {
 }
 
 function deleteAllCookies() {
+    Item_number = 0;
     const cookies = document.cookie.split(';');
 
     for (let i = 0; i < cookies.length; i++) {
@@ -21,15 +24,13 @@ function deleteAllCookies() {
     }
 }
 
-
-
 function deleteCookie(name) {
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
 }
 
-//deleteAllCookies(); //delete all cookies by default;
+deleteAllCookies(); //delete all cookies by default;
 
- console.log(document.cookie); // check current cookie
+console.log(document.cookie); // check current cookie
 // Attach click event listeners to buy buttons
 document.querySelectorAll('.buy').forEach(function(anchor) {
     anchor.addEventListener('click', function() {
@@ -40,15 +41,45 @@ document.querySelectorAll('.buy').forEach(function(anchor) {
         Price = parseFloat(document.querySelector(`.${Item} .price_number`).textContent);
         Item_name = document.querySelector(`.${Item} .item_name`).textContent;
 
-        // Log the updated values
-        console.log('Item:', Item);
-        console.log(`Price: ${Price}`);
-        console.log(`Item_name : ${Item_name}`);
+        // Increment the count for the current item
+        let itemCount = parseInt(getCookie(`Count_${Item_name}`)) || 0;
+        itemCount++;
+        setCookie(`Count_${Item_name}`, itemCount, 365);
 
         // Set cookies
         setCookie(`Price_${Item_name}`, Price, 365);
+        setCookie('Item_number', Item_number + 1, 365);
+        Item_number += 1;
+
+        // Log the updated values
+       // console.log('Item:', Item);
+        //console.log(`Price: ${Price}`);
+        //console.log(`Item_name : ${Item_name}`);
+        //console.log(`Count_${Item_name} : ${itemCount}`);
 
         // Log the current cookies
         console.log(document.cookie);
     });
+});
+
+function getCookie(name) {
+    console.log(`cookies before split: `,document.cookie);
+    const cookies = document.cookie.split(';');
+    console.log(`cookies after split: ${cookies}`);
+    console.log(`cookies length: ${cookies.length}`);
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        console.log(`cookies${i} : ${cookie}`);
+        if (cookie.startsWith(name + '=')) {
+            console.log(`name ${name.length}`);
+            console.log(`return cooki.substring: ${cookie.substring(name.length + 1)}`);
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
+}
+
+let opencart = document.querySelector(".cart-item");
+opencart.addEventListener('click', function() {
+    // Add code to handle the cart item click
 });
